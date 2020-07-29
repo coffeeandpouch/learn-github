b5b432606cee6010498a43d26f7c442182c789bf
SAM EINSIDLER <sameinsidler@SAMs-Air.attlocal.net>
2020-07-28 20:19:09 -0500

    latest changes

--- Documents/Simple GET Requests Assignment/index1.js
+++ Documents/Simple GET Requests Assignment/index1.js
@@ -1,31 +1,32 @@
-'use strict';
+"use strict";
 
 function getDogImage(someBreed) {
-    fetch(`https://dog.ceo/api/breed/${someBreed}/images/random`)
-        .then((response) => response.json())
-        .then((responseJson) => displayResults(responseJson))
-        .catch((error) => alert('Breed that you are looking for cannot be found'));
+  fetch(`https://dog.ceo/api/breed/${someBreed}/images/random`)
+    .then((response) => response.json())
+    .then((responseJson) => displayResults(responseJson))
+    //.catch((error) => alert("Breed that you are looking for cannot be found"));
+}
 
-function displayResults(responseJson) {
+  function displayResults(responseJson) {
     //map dog img to html tag
-    const breedPic = responseJson.message.map(
-        (url) => `<img src="${url}" class="results-img">`
+    const breedPic = responseJson.message(
+      (url) => `<img src="${url}" class="results-img">`
     );
     //replace existing image with a new one
     $("#dog").html(breedPic);
     $(".results").removeAttr("hidden");
-}
+  };
 
-function watchForm() {
+  function watchForm() {
     $("form").submit((event) => {
-        event.preventDefault();
-        var dogBreed = $("#breed").val();
-        getDogImage(dogBreed);
+      event.preventDefault();
+      var dogBreed = $("#breed").val();
+      getDogImage(dogBreed);
     });
-}
+  }
 
-$(function () {
+  $(function () {
     console.log("App loaded! Waiting for submit!");
     watchForm();
-});
-}
+  });
+
